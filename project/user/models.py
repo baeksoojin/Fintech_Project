b37acf13b7e36ffe537a282f8ae8b697b12456ce
sujin-create <qsoojinq@naver.com>
2022-08-06 22:10:28 +0900

    Fix : google로그인시에러로  phonenum model null=True로 수정

--- project/user/models.py
+++ project/user/models.py
@@ -5,8 +5,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=50)
-    phoneNumber = PhoneNumberField(unique = True, null = False, blank = False)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, null=True)
+    phoneNumber = PhoneNumberField(unique = True, null = True, blank = False)
     
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
