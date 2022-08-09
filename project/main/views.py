6934c5aa0bb722bf8380bf53f2e4cda053238d7c
sujin-create <qsoojinq@naver.com>
2022-08-10 03:12:10 +0900

    ADD : 오락마당 페이지 연결

--- project/main/views.py
+++ project/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def enjoy(request):
+    return render(request,'enjoy.html')
