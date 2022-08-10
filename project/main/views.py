a6a4fdc6158067ba8b332f54e87370fd5231000e
sujin-create <qsoojinq@naver.com>
2022-08-10 20:42:23 +0900

    ADD : page link 연결

--- project/main/views.py
+++ project/main/views.py
@@ -1,17 +1,54 @@
 from django.shortcuts import render
+from user.models import User
+import json
 
 # Create your views here.
+
+def get_family(request):
+    user = User
+    member = user.objects.get(phoneNumber=request.session.get('phoneNumber'))#본인
+    res_data={}
+
+    jsonDecoder= json.decoder.JSONDecoder()
+    family_list = jsonDecoder.decode(member.family)
+    print(family_list)
+
+    opponent = user.objects.get(phoneNumber=family_list[0])#상대방
+    opponent_name = opponent.username
+    if opponent.kind==False:
+        opponent_kind = "손주"
+    else:
+        opponent_kind = "조부모"
+
+    print(opponent_name)
+    res_data['opponent_name'] = opponent_name
+    res_data['opponent_kind'] = opponent_kind
+    return res_data
+    
 def home(request):
-    return render(request,'index.html')
+
+    res_data = get_family(request)
+
+    return render(request,'index.html',res_data)
 
 def enjoy(request):
+
+    res_data = get_family(request)
+
     return render(request,'enjoy.html')
 
 def pinmoney(request):
-    return render(request,'pinmoney.html')
+
+    res_data = get_family(request)
+
+    return render(request,'pinmoney.html', res_data)
 
 def mylogin(request):
+
     return render(request,'login_index.html')
 
 def mypage(request):
-    return render(request,'user/mypage.html')
+
+    res_data = get_family(request)
+
+    return render(request,'user/mypage.html',res_data)
