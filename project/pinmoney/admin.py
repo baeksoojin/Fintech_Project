1deeb7696752c6f3b8af11ca9b73277a502bcb14
sujin-create <qsoojinq@naver.com>
2022-08-12 01:36:38 +0900

    ADD : 정기용돈 등록 및 리스트 기능 추가

--- project/pinmoney/admin.py
+++ project/pinmoney/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from pinmoney.models import Pinmoney,Regular
 
 # Register your models here.
+admin.site.register(Pinmoney)
+admin.site.register(Regular)
--- project/pinmoney/urls.py
+++ project/pinmoney/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('transaction/',views.transaction,name='transaction'),
     path('certification/',views.certification,name='certification'),
     path('success/',views.success,name='success'),
+    path('regular/',views.regular,name="regular"),
+    path('regular_list/',views.Regular_list,name="regular_list"),
 ]
