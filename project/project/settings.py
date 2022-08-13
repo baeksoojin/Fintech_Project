6476104dd443ccf081d888de23ec4aaee4380eb3
jung <woorong98@gmail.com>
2022-08-13 21:47:15 +0900

    add voice

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from re import S
 import environ
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
---
+++ project/voice/main.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+# microphone에서 auido source를 생성합니다
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# 구글 웹 음성 API로 인식하기 (하루에 제한 50회)
+try:
+    print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# 결과
+# Google Speech Recognition thinks you said : 안녕하세요
